d12155db74ab0f92d2620ff6069dddc867c0eeb9
Alex Gisby <alex@solution10.com>
2013-03-22 09:20:23 +0000

    Added PDO and Session drivers, fleshed out Session

---
+++ Solution10/Auth/Driver/PDO.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Solution10\Auth\Driver;
+use Solution10\Auth\StorageDelegate as StorageDelegate;
+
+/**
+ * PDO Driver for Auth.
+ *
+ * Provides a reference implementation of the Auth StorageDelegate using PHP's
+ * PDO database abstraction layer. Can also be used in anger.
+ *
+ * @package 	Solution10
+ * @category 	Auth\Driver
+ * @author 		Alex Gisby <alex@solution10.com>
+ * @license 	MIT
+ */
+class PDO implements StorageDelegate
+{
+	// TBD
+}
---
+++ Solution10/Auth/Driver/Session.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Solution10\Auth\Driver;
+use Solution10\Auth\SessionDelegate as SessionDelegate;
+
+session_start();
+
+/**
+ * Native PHP Session driver for Auth.
+ *
+ * Provides a reference implementation of the SessionDelegate class.
+ * Uses built in $_SESSION for storage. Simples.
+ *
+ * @package 	Solution10
+ * @category 	Auth\Driver
+ * @author 		Alex Gisby <alex@solution10.com>
+ * @license 	MIT
+ */
+class Session implements SessionDelegate
+{
+	/**
+	 * Reads the authentication data out of the session for a given named instance.
+	 *
+	 * @param 	string 	Instance name
+	 * @return 	string 	Auth data string from the cookie / session etc
+	 */
+	public function auth_read($instance_name)
+	{
+		return (isset($_SESSION['s10auth'][$instance_name]))? 
+					$_SESSION['s10auth'][$instance_name] : 
+					false;
+	}
+
+	/**
+	 * Writes the authentication data into the session.
+	 * 
+	 * @param  string $instance_name Name of the Auth instance to write.
+	 * @param  string $auth_data     Encrypted data to write to the store.
+	 * @return bool 	True for success, false for failure.
+	 */
+	public function auth_write($instance_name, $auth_data)
+	{
+		$_SESSION['s10auth'][$instance_name] = $auth_data;
+		return true;
+	}
+
+	/**
+	 * Deletes a value from the persistent store
+	 *
+	 * @param  string 	$instance_name 	Name of the instance to void
+	 * @return bool
+	 */
+	public function auth_delete($instance_name)
+	{
+		unset($_SESSION['s10auth'][$instance_name]);
+		return true;
+	}
+}
